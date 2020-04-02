1e9d6fed690bc18d4d18842e820ce7efe74e0282
mmuqiitf <mmuqiit.f14@gmail.com>
2020-04-02 19:52:35 +0700

    Update beberapa ui color

--- news.js
+++ news.js
@@ -1,30 +1,29 @@
 function getNews() {
     $.ajax({
-        url:"http://newsapi.org/v2/everything?q=COVID-19&domains=detik.com&from=2020-03-01&sortBy=publishedAt&apiKey=3ea3b5c45dab4a8491123c8567648e72",
-        type:"GET",
-        success:function(response) {
+        url: "http://newsapi.org/v2/everything?q=COVID-19&domains=detik.com&from=2020-04-01&sortBy=publishedAt&apiKey=3ea3b5c45dab4a8491123c8567648e72",
+        type: "GET",
+        success: function (response) {
 
-            for(i=0;i<15;i++){
-                $('#newsPlace').append('<div class="bg-white col-12 col-lg-4 p-2" style="border-radius:.8rem;box-shadow:0 0 4px 0 rgba(0,0,0,.05)">'
-                +'<img class="card-img-top" src="'+response['articles'][i]['urlToImage']+'" alt="Card image cap">'
-                +'<div class="card-body">'
-                +'<div>'
-                +'<label>'+ response['articles'][i]['author'] +' / '+ response['articles'][i]['publishedAt'] +'</label>'
-                +'</div>'
-                +'<h5 class="card-title">'+ response['articles'][i]['title'] +'</h5>'
-                +'<p class="card-text">'+ response['articles'][i]['description'] +'</p>'
-                +'<a target="_blank" href="'+response['articles'][i]['url']+'" style="color: burlywood;">Baca Selengkapnya ...</a><br>'
-                +'</div>'
-                +'</div>');
+            for (i = 0; i < 15; i++) {
+                $('#newsPlace').append('<div class="bg-white col-12 col-lg-4 p-2" style="border-radius:.8rem;box-shadow:0 0 4px 0 rgba(0,0,0,.05)">' +
+                    '<img class="card-img-top" src="' + response['articles'][i]['urlToImage'] + '" alt="Card image cap">' +
+                    '<div class="card-body">' +
+                    '<div>' +
+                    '<label>' + response['articles'][i]['author'] + ' / ' + response['articles'][i]['publishedAt'] + '</label>' +
+                    '</div>' +
+                    '<h5 class="card-title">' + response['articles'][i]['title'] + '</h5>' +
+                    '<p class="card-text">' + response['articles'][i]['description'] + '</p>' +
+                    '<a target="_blank" href="' + response['articles'][i]['url'] + '" style="color: burlywood;">Baca Selengkapnya ...</a><br>' +
+                    '</div>' +
+                    '</div>');
             }
             setTimeout(
-            function() 
-            {
-                $( "#closeModal" ).click();
-            }, 3000);
+                function () {
+                    $("#closeModal").click();
+                }, 3000);
         },
-        error:function(){
+        error: function () {
             alert('error');
         }
-});
+    });
 }
